9de0ad38ab4f25719fcb82a0001df86e7ba3404b
Apoorv <apoorv.25007179@kiet.edu>
2026-02-20 23:27:47 +0530

    Grocery Billing

--- Assesmentryan.py
+++ Assesmentryan.py
@@ -1,12 +1,16 @@
-a=float(input())
-b=float(input())
-c=float(input())
-total=a+b+c
-if total>50:
-    discount=total*0.10
-else:
-    discount=0
-final=total-discount
-print("Original Total:",total)
-print("Discount:",discount)
-print("Final Amount:",final)
+item1 = float(input("Enter price of Item 1:$"))
+item2 = float(input("Enter price of Item 2:$"))
+item3 = float(input("Enter price of Item 3:$"))
+total = item1 + item2 + item3
+
+discount = 0
+
+if total > 50:
+    discount = total * 0.10
+final_amount = total - discount
+
+print("\n----- Billing Summary -----")
+print("Original Total: $", round(total, 2))
+print("Discount: $", round(discount, 2))
+print("Final Payable Amount: $", round(final_amount, 2))
+print("Thankyou Visit Again")
